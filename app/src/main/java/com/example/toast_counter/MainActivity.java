73091b8ff9850ff59c433bcc98a5b4eb13d5a348
Mohammed Tahir <tahirb541@gmail.com>
2023-09-18 08:43:22 +0500

    1st Commit

--- app/src/main/java/com/example/toast_counter/MainActivity.java
+++ app/src/main/java/com/example/toast_counter/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.toast_counter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int m_count=0;
+    private TextView m_showCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        m_showCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showCount(View view) {
+        m_count++;
+        m_showCount.setText(Integer.toString(m_count));
+    }
+
+    public void makeToast(View view) {
     }
 }
